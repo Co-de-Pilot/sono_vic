af39e2d7fdde7f191f75d268fa4a7fddd5991e7a
CO_DE_PILOT <piratosnogi@gmail.com>
2025-05-23 11:27:57 +0200

    Menu operable + SVGs

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("menu-active");
   menuList.classList.toggle("menu-active");
 });
+
+/* MENU ACTIVE LINK vezérlése */
+const sections = document.querySelectorAll("section");
+const menuitems = document.querySelectorAll("nav .menu li a.menu-ref");
+
+menuitems.forEach((menuitem, index) => {
+  menuitem.addEventListener("click", (menuevent) => {
+    menuevent.preventDefault();
+    let actualmenuitem = document.querySelector(".actual");
+    actualmenuitem.classList.remove("actual");
+    menuitem.classList.add("actual");
+    let actualsection = document.querySelector(".actualsection");
+    actualsection.classList.remove("actualsection");
+    sections[index].classList.add("actualsection");
+  });
+});
